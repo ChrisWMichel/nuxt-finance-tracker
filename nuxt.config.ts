daa0cd1622b90ea44cab221311f3309efb707877
ChrisWMichel <christopherw.michel@gmail.com>
2024-05-07 11:17:15 -0400

    fix login bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
-    redirect: true
+    redirect: false
   },
   runtimeConfig: {
     public: {
