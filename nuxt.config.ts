4d03938031f0b24c9d9469b22c53934186165f5a
ChrisWMichel <christopherw.michel@gmail.com>
2024-05-07 16:20:26 -0400

    updating nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
-    redirect: false
+    redirect: true
   },
   runtimeConfig: {
     public: {
